63121fa6ff20c2dae85d4afcae66cb7d876f94ce
Paul Graham <paul@bytefair.com>
2015-05-09 20:28:57 -0500

    basic app setup

---
+++ app.js
@@ -0,0 +1,29 @@
+'use strict'
+
+// environment setup
+require('dotenv').load()
+
+// mongoose
+const mongoose = require('mongoose')
+mongoose.connect(process.env.MONGO)
+mongoose.connection.on('error', function(err) {
+  console.log(err)
+})
+
+// express setup
+const express = require('express')
+    , serveStatic = require('serve-static')
+    , compression = require('compression')
+
+var app = express()
+
+app.use(compression({ threshold: 1 }))
+
+// helmet config
+const helmet = require('helmet')
+app.use(helmet())
+
+// start the app
+app.listen(process.env.PORT, function() {
+  console.log('Relay listening on port ' + process.env.PORT)
+})
